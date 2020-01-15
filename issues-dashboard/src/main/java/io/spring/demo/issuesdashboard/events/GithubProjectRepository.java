587559afac0d9868408f55552ff81406f82d90bf
Pamela Detroyat <pamela.detroyat@gmail.com>
2020-01-15 11:21:09 +0100

    Add skeleton from spring boot

---
+++ issues-dashboard/src/main/java/io/spring/demo/issuesdashboard/events/GithubProjectRepository.java
@@ -0,0 +1,8 @@
+package io.spring.demo.issuesdashboard.events;
+
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+public interface GithubProjectRepository extends PagingAndSortingRepository<GithubProject, Long> {
+
+	GithubProject findByRepoName(String repoName);
+}
---
+++ issues-dashboard/src/test/java/io/spring/demo/issuesdashboard/IssuesDashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package io.spring.demo.issuesdashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IssuesDashboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
