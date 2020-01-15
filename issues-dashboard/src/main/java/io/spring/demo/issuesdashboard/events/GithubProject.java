d47a257ad6ac33cecbdb47a4d1924250a818a9fb
Pamela Detroyat <pamela.detroyat@gmail.com>
2020-01-15 14:51:11 +0100

    Add github api part

--- issues-dashboard/src/main/java/io/spring/demo/issuesdashboard/events/GithubProject.java
+++ issues-dashboard/src/main/java/io/spring/demo/issuesdashboard/events/GithubProject.java
@@ -7,6 +7,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
+@SuppressWarnings("serial")
 @Entity
 public class GithubProject implements Serializable {
 
